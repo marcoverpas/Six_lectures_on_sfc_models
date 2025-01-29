93720b87854d77b333b300ee4eb6290c11e0ec24
Marco Veronese Passarella <marco.veronese.passarella@gmail.com>
2025-01-29 17:49:26 +0100

    Update BMW_model.R

--- BMW_model.R
+++ BMW_model.R
@@ -3,7 +3,7 @@
 # Monetary Economics
 # Chapter 7 
 
-# Version: 30 May 2019; revised: 6 November 2022
+# Version: 30 May 2019; revised: 29 Jamuary 2025
 
 #STEP 1: PREPARE THE WORK-SPACE ################################################################################
 
@@ -23,41 +23,41 @@ nPeriods = 150
 nScenarios=6 
 
 #STEP 2: SET THE COEFFICIENTS (EXOGENOUS VARIABLES AND PARAMETERS) ################################################################################
-alpha2=0.1 #Propensity to consume out of wealth
-delta=0.1 #Depreciation rate
-gamma=0.15 #Reaction speed of adjustment of capital to its target value
-alpha1r = 0.5 #Propensity to consume out of interests if c_d=c_d(r)
-alpha1w = 0.75 #Propensity to consume out of wages if c_d=c_d(r)
-alpha0=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Set autonomous component of consumption as a matrix (for shocks) 
-alpha1=matrix(data=0.75,nrow=nScenarios,ncol=nPeriods) #Set autonomous propensity to consume out of income as a matrix (for shocks) 
-kappa=matrix(data=1,nrow=nScenarios,ncol=nPeriods)  #Capital-output ratio
-rl_bar=matrix(data=0.04,nrow=nScenarios,ncol=nPeriods)  #Rate of interests on bank loans - exogenously set
+alpha2 <- 0.1 #Propensity to consume out of wealth
+delta <- 0.1 #Depreciation rate
+gamma <- 0.15 #Reaction speed of adjustment of capital to its target value
+alpha1r <- 0.5 #Propensity to consume out of interests if c_d=c_d(r)
+alpha1w <- 0.75 #Propensity to consume out of wages if c_d=c_d(r)
+alpha0 <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Set autonomous component of consumption as a matrix (for shocks) 
+alpha1 <- matrix(data=0.75,nrow=nScenarios,ncol=nPeriods) #Set autonomous propensity to consume out of income as a matrix (for shocks) 
+kappa <- matrix(data=1,nrow=nScenarios,ncol=nPeriods)  #Capital-output ratio
+rl_bar <- matrix(data=0.04,nrow=nScenarios,ncol=nPeriods)  #Rate of interests on bank loans - exogenously set
 
 #STEP 3: CREATE VARIABLES AND ATTRIBUTE INITIAL VALUES ################################################################################
-af=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Create variables and attribute values to stocks
-c_d=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Consumption goods demand by households
-c_s=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Consumption goods supply
-da=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Depreciation allowances
-k=matrix(data=0,nrow=nScenarios,ncol=nPeriods)  #Stock of capital
-kt=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Target stock of capital
-l_d=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Demans for bank loans 
-l_s=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of bank loans 
-i_d=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Demand for Investment
-i_s=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of Investment
-mh=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Bank deposits held by households
-ms=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of bank deposits
-n_s=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of labour
-n_d=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Demand for labour
-pr=matrix(data=1,nrow=nScenarios,ncol=nPeriods) #Labour productivity
-rl=matrix(data=rl_bar,nrow=nScenarios,ncol=nPeriods) #Rate of interests on banks loans
-rm=matrix(data=rl,nrow=nScenarios,ncol=nPeriods) #Rate of interests on bank deposits
-w=matrix(data=0.86,nrow=nScenarios,ncol=nPeriods) #Wage rate
-wb_d=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Wage Bill
-wb_s=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of Wages
-y=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Income
-y_star=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Steady-state income
-yd=matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Disposal Income of households
-       
+af <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Create variables and attribute values to stocks
+c_d <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Consumption goods demand by households
+c_s <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Consumption goods supply
+da <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Depreciation allowances
+k <- matrix(data=0,nrow=nScenarios,ncol=nPeriods)  #Stock of capital
+kt <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Target stock of capital
+l_d <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Demans for bank loans 
+l_s <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of bank loans 
+i_d <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Demand for Investment
+i_s <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of Investment
+mh <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Bank deposits held by households
+ms <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of bank deposits
+n_s <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of labour
+n_d <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Demand for labour
+pr <- matrix(data=1,nrow=nScenarios,ncol=nPeriods) #Labour productivity
+rl <- matrix(data=rl_bar,nrow=nScenarios,ncol=nPeriods) #Rate of interests on banks loans
+rm <- matrix(data=rl,nrow=nScenarios,ncol=nPeriods) #Rate of interests on bank deposits
+w <- matrix(data=0.86,nrow=nScenarios,ncol=nPeriods) #Wage rate
+wb_d <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Wage Bill
+wb_s <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Supply of Wages
+y <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Income
+y_star <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Steady-state income
+yd <- matrix(data=0,nrow=nScenarios,ncol=nPeriods) #Disposal Income of households
+
 #STEP 4: RUN THE MODEL ################################################################################
 
 #STEP 4.A: DEFINE THE LOOPS
@@ -98,80 +98,80 @@ for (j in 1:nScenarios){
       
       #Behavioural equations and equilibrium condition
       
-        #Supply of consumption goods - eq. 7.1
-        c_s[j,i] = c_d[j,i]
-        #Supply of investment goods - eq. 7.2
-        i_s[j,i] = i_d[j,i]
-        #Supply of labour - eq. 7.3
-        n_s[j,i] = n_d[j,i]
-        #Supply of loans - eq. 7.4
-        l_s[j,i] = l_s[j,i-1] + (l_d[j,i] - l_d[j,i-1])   
+      #Supply of consumption goods - eq. 7.1
+      c_s[j,i] = c_d[j,i]
+      #Supply of investment goods - eq. 7.2
+      i_s[j,i] = i_d[j,i]
+      #Supply of labour - eq. 7.3
+      n_s[j,i] = n_d[j,i]
+      #Supply of loans - eq. 7.4
+      l_s[j,i] = l_s[j,i-1] + (l_d[j,i] - l_d[j,i-1])   
       
       #Transactions of the firms
       
-        #GDP - eq. 7.5
-        y[j,i] = c_s[j,i] + i_s[j,i]
-        #Wage bill - eq. 7.6
-        wb_d[j,i] = y[j,i] - rl[j,i-1]*l_d[j,i-1] - af[j,i]   
-        #Depreciation allowances - eq. 7.7
-        af[j,i] = da[j,i]
-        #Demand for bank loans - eq. 7.8
-        l_d[j,i] = l_d[j,i-1] + i_d[j,i] - af[j,i] 
+      #GDP - eq. 7.5
+      y[j,i] = c_s[j,i] + i_s[j,i]
+      #Wage bill - eq. 7.6
+      wb_d[j,i] = y[j,i] - rl[j,i-1]*l_d[j,i-1] - af[j,i]   
+      #Depreciation allowances - eq. 7.7
+      af[j,i] = da[j,i]
+      #Demand for bank loans - eq. 7.8
+      l_d[j,i] = l_d[j,i-1] + i_d[j,i] - af[j,i] 
       
       #Transactions of households
       
-        #Disposable income - eq. 7.9
-        yd[j,i] = wb_d[j,i] + rm[j,i-1]*mh[j,i-1]
+      #Disposable income - eq. 7.9
+      yd[j,i] = wb_d[j,i] + rm[j,i-1]*mh[j,i-1]
       
-        #Bank deposits held by households - eq. 7.10
-        mh[j,i] = mh[j,i-1] + yd[j,i] - c_d[j,i]
+      #Bank deposits held by households - eq. 7.10
+      mh[j,i] = mh[j,i-1] + yd[j,i] - c_d[j,i]
       
       #Transactions of the banks  
       
-        #Supply of deposits - eq. 7.11
-        ms[j,i] = ms[j,i-1] + (l_s[j,i] - l_s[j,i-1])
+      #Supply of deposits - eq. 7.11
+      ms[j,i] = ms[j,i-1] + (l_s[j,i] - l_s[j,i-1])
       
-        #Rate of interest on deposits - eq. 7.12
-        rm[j,i] = rl[j,i]
+      #Rate of interest on deposits - eq. 7.12
+      rm[j,i] = rl[j,i]
       
       #The wage bill  
       
-        #"Supply" of wages - eq. 7.13
-        wb_s[j,i] = w[j,i]*n_s[j,i]         
+      #"Supply" of wages - eq. 7.13
+      wb_s[j,i] = w[j,i]*n_s[j,i]         
       
-        #Labour demand - eq. 7.14
-        n_d[j,i] = y[j,i]/pr[j,i]        
+      #Labour demand - eq. 7.14
+      n_d[j,i] = y[j,i]/pr[j,i]        
       
-        #Wage rate - eq. 7.15
-        w[j,i] = wb_d[j,i]/n_d[j,i]       
+      #Wage rate - eq. 7.15
+      w[j,i] = wb_d[j,i]/n_d[j,i]       
       
       #Household behaviour
       
-        #Demand for consumption goods - eq. 7.16 (and 7.16A)
-        if (j==5){ c_d[j,i] = alpha0[j,i] + alpha1w*wb_d[j,i] + alpha1r*rm[j,i-1]*mh[j,i-1] + alpha2*mh[j,i-1]}
-             else{ c_d[j,i] = alpha0[j,i] + alpha1[j,i]*yd[j,i] + alpha2*mh[j,i-1]}
+      #Demand for consumption goods - eq. 7.16 (and 7.16A)
+      if (j==5){ c_d[j,i] = alpha0[j,i] + alpha1w*wb_d[j,i] + alpha1r*rm[j,i-1]*mh[j,i-1] + alpha2*mh[j,i-1]}
+      else{ c_d[j,i] = alpha0[j,i] + alpha1[j,i]*yd[j,i] + alpha2*mh[j,i-1]}
       
       #The investment behaviour
       
-        #Accumulation of capital - eq. 7.17
-        k[j,i] = k[j,i-1] + i_d[j,i] - da[j,i]
+      #Accumulation of capital - eq. 7.17
+      k[j,i] = k[j,i-1] + i_d[j,i] - da[j,i]
       
-        #Depreciation allowances - eq. 7.18
-        da[j,i] = delta*k[j,i-1]
+      #Depreciation allowances - eq. 7.18
+      da[j,i] = delta*k[j,i-1]
       
-        #Capital stock target - eq. 7.19
-        kt[j,i] = kappa[j,i]*y[j,i-1]
+      #Capital stock target - eq. 7.19
+      kt[j,i] = kappa[j,i]*y[j,i-1]
       
-        #Demand for investment goods - eq. 7.20
-        i_d[j,i] = gamma*(kt[j,i] - k[j,i-1]) + da[j,i]
+      #Demand for investment goods - eq. 7.20
+      i_d[j,i] = gamma*(kt[j,i] - k[j,i-1]) + da[j,i]
       
       #The behaviour of banks
       
-        #Interest rate on loans - eq. 7.21
-        rl[j,i] = rl_bar[j,i]
+      #Interest rate on loans - eq. 7.21
+      rl[j,i] = rl_bar[j,i]
       
       #Additional equation for calculating y steady-state value
-        y_star[j,i] = alpha0[j,i]/((1-alpha1[j,i])*(1-delta*kappa[j,i])-alpha2*kappa[j,i])       
+      y_star[j,i] = alpha0[j,i]/((1-alpha1[j,i])*(1-delta*kappa[j,i])-alpha2*kappa[j,i])       
       
     }
   }
@@ -189,7 +189,7 @@ if ( aerror<0.01 ){cat(" *********************************** \n Good news! The m
   else{cat(" ******************************************* \n Warning: the model is not fully consistent! \n", "Average error =", aerror, "> 1 \n", "Cumulative error =", error, "\n *******************************************")} }      
 
 #Plot redundant equation
-layout(matrix(c(1,2,3,4,5,6,7,8,9), 3, 3, byrow = TRUE))
+layout(matrix(c(1), 1, 1, byrow = TRUE))
 par(mar = c(5.1+1, 4.1+1, 4.1+1, 2.1+1))
 plot(ms[1,2:nPeriods]-mh[1,2:nPeriods], type="l", col="green",lwd=3,lty=1,font.main=1,cex.main=1.5,
      main=expression("Consistency check (baseline scenario): " * italic(M[phantom("")]["s"]) - italic(M[phantom("")]["h"])),
@@ -199,7 +199,7 @@ plot(ms[1,2:nPeriods]-mh[1,2:nPeriods], type="l", col="green",lwd=3,lty=1,font.m
 #STEP 6: CREATE AND DISPLAY GRAPHS ################################################################################
 
 #Change layout
-#layout(matrix(c(1,2), 1, 2, byrow = TRUE))
+layout(matrix(c(1,2,3,4,5,6), 3, 2, byrow = TRUE))
 par(mar = c(5.1+1, 4.1+1, 4.1+1, 2.1+1))
 
 #Figure 1
